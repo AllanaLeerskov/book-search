4db7edd8ab5a39e24ede2fb4ef5f888bc5503f35
Elektra <allanaleerskov@gmail.com>
2022-03-29 14:53:37 -0600

    apollo server in server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require('./utils/auth');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const server = newApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -15,6 +26,11 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.use(routes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
